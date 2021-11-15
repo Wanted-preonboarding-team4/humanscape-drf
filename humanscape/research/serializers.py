dea7d23d39203d79713074cec42b4955f206913d
damin0320 <damin0320@gmail.com>
2021-11-16 00:28:15 +0900

    Feature : research serializers 추가

--- humanscape/research/serializers.py
+++ humanscape/research/serializers.py
@@ -1,1 +1,8 @@
 from rest_framework import serializers
+from .models import Research
+
+class ResearchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Research
+        fields = ('id', 'subject_number', 'subject_name', 'test_subject', 'study_period', 'department', 'research_institution',
+                  'research_type', 'research_step', 'research_scope')
