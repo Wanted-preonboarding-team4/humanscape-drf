76f84bb616396f845489af07a6896508d6a08dc9
damin0320 <damin0320@gmail.com>
2021-11-16 01:33:12 +0900

    Feature : API 통해 임상정보 수집 기능 구현

--- humanscape/research/views.py
+++ humanscape/research/views.py
@@ -3,44 +3,55 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from .serializers import ResearchSerializer
 from .models import Research, Department, ResearchInstitution, ResearchType, ResearchStep, ResearchScope
-import csv
 
-file_dir = '/Users/daminan/Desktop/humanscape-drf/'
+from urllib.request import urlopen
+import urllib
+import json
+import ssl
 
 
-def read_data(table_name):
-    with open(file_dir + f'{table_name}.csv', 'r', encoding='CP949') as csvfile:
-        reader = csv.reader(csvfile)
-        data = list(reader)[1:]
-    # data = ApiResponse.api_response()
+def api_response():
+    context = ssl._create_unverified_context()
+    url = "https://api.odcloud.kr/api/3074271/v1/uddi:cfc19dda-6f75-4c57-86a8-bb9c8b103887?page=1&perPage=145&serviceKey="
+    encode = "SFxCsx3sxrgFTEUnr4Nz5hKzR9TIF%2FVnA3ekvLJYmIA2CtW%2FoQzet7%2FWebSVNzhlP09pKB8X5Z69MUG55c1gNw%3D%3D"
+    res = urllib.request.urlopen(url + encode, context=context)
+    json_str = res.read().decode("utf-8")
+    json_object = json.loads(json_str)
+    return json_object['data']
+
+
+def read_data():
+    data = api_response()
     for datum in data:
-        if not datum[4]:
-            datum[4] = 0
-        department = Department.objects.filter(name=datum[2]).first()
+        if not datum["전체목표연구대상자수"]:
+            datum["전체목표연구대상자수"] = 0
+        department = Department.objects.filter(name=datum["진료과"]).first()
         if not department:
-            department = Department.objects.create(name=datum[2])
-        research_institution = ResearchInstitution.objects.filter(name=datum[3]).first()
+            department = Department.objects.create(name=datum["진료과"])
+        research_institution = ResearchInstitution.objects.filter(name=datum["연구책임기관"]).first()
         if not research_institution:
-            research_institution = ResearchInstitution.objects.create(name=datum[3])
-        research_type = ResearchType.objects.filter(name=datum[6]).first()
+            research_institution = ResearchInstitution.objects.create(name=datum["연구책임기관"])
+        research_type = ResearchType.objects.filter(name=datum["연구종류"]).first()
         if not research_type:
-            research_type = ResearchType.objects.create(name=datum[6])
-        research_step = ResearchStep.objects.filter(name=datum[7]).first()
+            research_type = ResearchType.objects.create(name=datum["연구종류"])
+        research_step = ResearchStep.objects.filter(name=datum["임상시험단계(연구모형)"]).first()
         if not research_step:
-            research_step = ResearchStep.objects.create(name=datum[7])
-        research_scope = ResearchScope.objects.filter(name=datum[8]).first()
+            research_step = ResearchStep.objects.create(name=datum["임상시험단계(연구모형)"])
+        research_scope = ResearchScope.objects.filter(name=datum["연구범위"]).first()
         if not research_scope:
-            research_scope = ResearchScope.objects.create(name=datum[8])
-        research = Research.objects.filter(subject_number=datum[0])
+            research_scope = ResearchScope.objects.create(name=datum["연구범위"])
+        research = Research.objects.filter(subject_number=datum["과제번호"])
         if not research:
-            Research.objects.create(subject_number=datum[0], subject_name=datum[1], department=department,
+            Research.objects.create(subject_number=datum["과제번호"], subject_name=datum["과제명"], department=department,
                                     research_institution=research_institution, research_type=research_type,
                                     research_step=research_step,
-                                    research_scope=research_scope, study_period=datum[5], test_subject=datum[4])
+                                    research_scope=research_scope, study_period=datum["연구기간"],
+                                    test_subject=datum["전체목표연구대상자수"])
     return
 
 
 class ResearchView(APIView):
     def get(self, request):
-        read_data("research_data")
-        return Response(status=200)
+        research_data_list = Research.objects.all()
+        serializer = ResearchSerializer(research_data_list, many=True)
+        return Response(serializer.data, status=200)
