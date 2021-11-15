633617eb37a30502fb961e3b7e010a9cd0a0ecea
damin0320 <damin0320@gmail.com>
2021-11-16 00:29:06 +0900

    ADD : GET research API 경로 설정

---
+++ humanscape/research/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.conf.urls import url,include
+from django.contrib import admin
+from rest_framework import routers
+# from research.views import ResearchViewSet
+import research.views
+from research.views import ResearchView
+
+
+# router = routers.DefaultRouter()
+# router.register('research', ResearchView)
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    path('research/', research.views.ResearchView.as_view(),)
+]
