2a0ec64d556305b18696d5a097c4a80b3a1f3f39
damin0320 <damin0320@gmail.com>
2021-11-17 00:00:46 +0900

    Test : Trial list test 구현 - 성공한 경우 200 반환하는지 - 페이지네이션 작동 하는지 - 일주일 내 업데이트 내용인지

--- humanscape/research/tests.py
+++ humanscape/research/tests.py
@@ -1,3 +1,67 @@
-from django.test import TestCase
+from unittest.mock import patch
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import Trial, Department, ResearchInstitution, ResearchType, ResearchStep, ResearchScope
+
+from datetime import datetime, timedelta
+
+
+class TrialListTestCase(APITestCase):
+    def setUp(self):
+        self.test_response ={"count": 1, "next": None, "previous": None, "results": [
+            {"id": 1, "trial_id": "C131010", "trial_name": "test", "total_target_number": 200,
+             "study_period": "3년", "department": 1, "research_institution": 1, "research_type": 1,
+             "research_step": 1, "research_scope": 1}]}
+
+        self.test_department = Department.objects.create(name="Hematology")
+        self.test_research_institution = ResearchInstitution.objects.create(name="서울아산병원")
+        self.test_research_type = ResearchType.objects.create(name="관찰연구")
+        self.test_research_step = ResearchStep.objects.create(name="코호트")
+        self.teat_research_scope = ResearchScope.objects.create(name="단일기관")
+
+        self.test_trial1 = Trial.objects.create(trial_id="C131010", trial_name="test", total_target_number=200,
+                                                study_period="3년",
+                                                department=self.test_department,
+                                                research_institution=self.test_research_institution,
+                                                research_type=self.test_research_type,
+                                                research_step=self.test_research_step
+                                                , research_scope=self.teat_research_scope)
+
+    def test_get_list_success(self):
+        response = self.client.get(f'/list/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_get_list_pagination(self):
+        for i in range(11):
+            Trial.objects.create(trial_id=f"C131010{i}", trial_name="test", total_target_number=200,
+                                                study_period="3년",
+                                                department=self.test_department,
+                                                research_institution=self.test_research_institution,
+                                                research_type=self.test_research_type,
+                                                research_step=self.test_research_step
+                                                , research_scope=self.teat_research_scope)
+
+        response = self.client.get(f'/list/')
+        self.assertEqual(len(response.json()["results"]), 10)
+
+    def test_get_list_time_limit(self):
+        for i in range(11):
+            Trial.objects.create(trial_id=f"C131010{i}", trial_name="test", total_target_number=200,
+                                                study_period="3년",
+                                                department=self.test_department,
+                                                research_institution=self.test_research_institution,
+                                                research_type=self.test_research_type,
+                                                research_step=self.test_research_step,
+                                                research_scope=self.teat_research_scope,
+                                                updated_at=datetime.now()-timedelta(days=8)
+                                 )
+
+        response = self.client.get(f'/list/')
+        response_json = response.json()["results"]
+        update_date = response_json[0]["updated_at"][:10]
+        self.assertEqual(len(response_json), 1)
+        self.assertGreater(datetime.strptime(update_date, '%Y-%m-%d'), datetime.now()-timedelta(days=7))
+
+
